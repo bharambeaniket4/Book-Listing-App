448d88d49890261450a1ee357715799a87673d36
Aniket <bharambeaniket4@gmail.com>
2024-12-22 17:57:08 +0530

    Fetching Feature Added

--- BookListingApp/force-app/main/default/lwc/bookApp/bookApp.js
+++ BookListingApp/force-app/main/default/lwc/bookApp/bookApp.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
+const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-export default class BookApp extends LightningElement {}
+export default class BookApp extends LightningElement {
+
+
+    searchKey = 'Man';
+
+    connectedCallback(){
+        this.fetchBookData();
+    }
+
+    fetchBookData(){
+        fetch( this.BOOK_URL + this.searchKey)
+        .then( response => response.json())
+        .then(data =>console.log(data))
+        .catch( error => console.error('Error :'+ error))
+    }
+
+
+}
